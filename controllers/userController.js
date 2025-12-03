73fb779391d5eaf8e8571a7cdcd2077b0a056de0
Sanashaikh8804 <sanashaikh8804@gmail.com>
2025-12-03 22:49:51 +0530

    authentication done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,44 @@
+const bcrypt = require("bcryptjs");
 const asyncHandler= require("express-async-handler");
+const User = require("../models/userModel");
 
-//Register a user
-//POST /api/users/register
+// Register a user
+// POST /api/users/register
 const registerUser= asyncHandler(async(req, res)=> {
-    const {email, password}= req.body;
-    if(!email || !password) {
+    const {name, email, password}= req.body;
+    if(!name || !email || !password) {
         res.status(400);
         throw new Error("All fields are mandatory!");
     }
-    const userAvailable= await UserActivation.findone({email});
+
+    const userAvailable= await User.findOne({email});
     if(userAvailable) {
         res.status(400);
         throw new Error("User already registered");
     }
-    
-    res.json({message: "Register the user"});
+
+    const hashedPassword= await bcrypt.hash(password, 10);
+    console.log('registerUser - hashedPassword:', hashedPassword);
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    });
+
+    // avoid returning the password field in the response
+    const userObj = user.toObject();
+    delete userObj.password;
+    console.log(`user created ${user}`);
+    if(user) {
+        res.status(201).json({_id: user._id, email: user.email});
+    }
+    else {
+        res.status(400);
+        throw new Error("User data is not valid");
+        
+    }
+
+    res.status(201).json({message: "User registered successfully", user: userObj});
 });
 
 const loginUser= asyncHandler(async(req, res)=> {

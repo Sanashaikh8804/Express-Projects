7f9e1425c58f68d762a4e230b8dfed2c5d7d1616
Sanashaikh8804 <sanashaikh8804@gmail.com>
2025-12-04 17:49:47 +0530

    added jw token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const asyncHandler= require("express-async-handler");
 const User = require("../models/userModel");
 
@@ -42,6 +43,31 @@ const registerUser= asyncHandler(async(req, res)=> {
 });
 
 const loginUser= asyncHandler(async(req, res)=> {
+    const {email, password}= req.body;
+    if(!email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+    const user= await User.findOne({email});
+    // compare password with hashed password
+    if(user && (await bcrypt.compare(password, user.password))) {
+        const accessToken= jwt.sign(
+            {
+                user: {
+                    username: user.name,
+                    email: user.email,
+                    id: user._id
+                },
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            {expiresIn: "1m"}
+        );
+        res.status(200).json({accessToken});
+    }else {
+        res.status(401);
+        throw new Error("Email or password is not valid");
+    }
+        res.status(200).json({accessToken});
     res.json({message: "Login the user"});
 });
 
